a6a6f6bf09f2400b5f063a0115fdc39b08dc6c23
sameturkarslan <sameturkarslan@gmail.com>
2026-07-11 00:10:02 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,9 +1,8 @@
 const gallery = document.getElementById("photoGallery");
 
 const cloudName = "p0wgo1yp";
-const tag = "buse_samet";
 
-fetch(`https://res.cloudinary.com/${cloudName}/image/list/${tag}.json`)
+fetch(`https://res.cloudinary.com/${cloudName}/image/list/buse_samet.json`)
   .then(response => response.json())
   .then(data => {
 
@@ -14,6 +13,10 @@ fetch(`https://res.cloudinary.com/${cloudName}/image/list/${tag}.json`)
       const img = document.createElement("img");
 
       img.src = `https://res.cloudinary.com/${cloudName}/image/upload/${photo.public_id}.${photo.format}`;
+
+      img.style.width = "100%";
+      img.style.borderRadius = "15px";
+      img.style.marginBottom = "15px";
 
       gallery.appendChild(img);
 
