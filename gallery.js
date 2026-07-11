2aef63d51bafb38f668e1ce2813dd59a86ff4095
sameturkarslan <sameturkarslan@gmail.com>
2026-07-11 20:55:57 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,32 +1,12 @@
-const gallery = document.getElementById("photoGallery");
+/*
+===========================================
+gallery.js
+===========================================
 
-const cloudName = "p0wgo1yp";
+Bu projede galeri yönetimi script.js
+içerisindeki Firestore onSnapshot()
+tarafından yapılmaktadır.
 
-fetch(`https://res.cloudinary.com/${cloudName}/image/list/buse_samet.json`)
-  .then(response => response.json())
-  .then(data => {
+Bu dosya bilinçli olarak boş bırakılmıştır.
 
-    gallery.innerHTML = "";
-
-    data.resources.forEach(photo => {
-
-      const img = document.createElement("img");
-
-      img.src = `https://res.cloudinary.com/${cloudName}/image/upload/${photo.public_id}.${photo.format}`;
-
-      img.style.width = "100%";
-      img.style.borderRadius = "15px";
-      img.style.marginBottom = "15px";
-
-      gallery.appendChild(img);
-
-    });
-
-  })
-  .catch(error => {
-
-    gallery.innerHTML = "Fotoğraflar yüklenemedi.";
-
-    console.log(error);
-
-  });
+*/
